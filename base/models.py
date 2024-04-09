bd1de194f244b4e0d041580a032330a9c1374d33
kmashao023 <kmashao023@student.wethinkcode.co.za>
2024-04-09 19:20:12 +0200

    fixing models in tasklist class

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class tasklist(models.Model):
     user_auth = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
